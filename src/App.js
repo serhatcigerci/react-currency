8ec5f2670dd42cbb2915d4d66da1e11729b9ba7d
serhatcigerci <yserhatcigerci@gmail.com>
2023-08-03 15:45:52 +0300

    Axios auth is entered but there are errors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
+import React, { useEffect } from 'react'
 import './App.css';
 import CurrencyRow from './CurrencyRow';
+import axios from 'axios'
+
+
+const BASE_URL = 'https://api.exchangeratesapi.io/v1/'
+const API_KEY = 'f03e227fa292c1250fd75c3cedf6c0d0'
+
+
 function App() {
+
+  useEffect(() => {
+     axios.get(BASE_URL, {}, {
+      auth: {
+        access_key: API_KEY
+      }
+     }).then(res => res.json()).then(data => console.log(data))  
+  }, [])
+  
   return (
     <>
       <h1>convert</h1>
