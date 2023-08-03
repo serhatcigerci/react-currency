9e9ec06f469a3a9f688c07738e5af8030b0dd763
serhatcigerci <yserhatcigerci@gmail.com>
2023-08-03 16:05:44 +0300

    UseState for currencyOptions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './App.css';
 import CurrencyRow from './CurrencyRow';
-
 
 
 const BASE_URL = 'https://api.exchangerate.host/latest'
@@ -9,8 +8,13 @@ const BASE_URL = 'https://api.exchangerate.host/latest'
 
 function App() {
 
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  console.log(currencyOptions)
+
   useEffect(() => {
-     fetch(BASE_URL).then(res => res.json()).then(data => console.log(data))  
+     fetch(BASE_URL).then(res => res.json()).then(data => {
+        setCurrencyOptions([data.base, ...Object.keys(data.rates)])
+      })  
   }, [])
   
   return (
